2a78b8f0af640f98c4bdea7bebb0269fd5f87579
Paul Wong <paulwcy@gmail.com>
2019-06-14 16:25:38 +1000

    added bupa background images

--- modules/Cards/BupaCard.js
+++ modules/Cards/BupaCard.js
@@ -1,116 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, Text, View } from 'react-native';
-import FlipCard from 'react-native-flip-card';
 
-import commonStyles from '../../common.styles';
-import bupaCardStyles from './styles/bupaCard.styles';
-import cardImages from './card.images';
-import { getFocusStyle } from '../../utils/textFocus';
+import GenericCard from './GenericCard';
 
-const backAttributes = ['issueNumber'];
+/*eslint-disable*/
+const defaultLogo = 'data:image/png;base64,';
+/*eslint-enable*/
 
-export const BupaCard = ({
-  cardHolderName,
-  issueNumber,
-  cardStyle,
-  width,
-  height,
-  focus,
-  memberNumber,
-  rank,
-  showBack,
-  showCardHolderName,
-  showIssueNumber,
-  showMembershipNumber,
-  showRank,
-  showSwipeBar,
-}) => (
-    <FlipCard
-      style={[cardStyle, bupaCardStyles.bupaBackgroundColor]}
-      friction={6}
-      perspective={1000}
-      flipVertical={false}
-      flip={showBack || backAttributes.includes(focus)}
-      flipHorizontal
-      clickable
-    >
-      <View style={[commonStyles.front, { width, height, flexDirection: 'column', justifyContent: 'space-between' }]}>
-        <View style={[commonStyles.row, { justifyContent: 'flex-end' }]}>
-          {
-            showRank &&
-            <View style={{ flex: 1, flexDirection: 'row', marginTop: 20 }}>
-              <Text style={[getFocusStyle(focus, 'rank', '#828282')]}>{rank}</Text>
-              {
-                showCardHolderName &&
-                <Text
-                  style={[getFocusStyle(focus, 'cardHolderName', '#828282'), {
-                    marginLeft: 10,
-                    marginTop: 2,
-                    fontSize: 12,
-                  }]}
-                  numberOfLines={1}
-                >
-                  {cardHolderName}
-                </Text>
-              }
-            </View>
-          }
-          <Image source={{ uri: cardImages.bupa.logoUri }} style={bupaCardStyles.logo} />
-        </View>
-        <View style={[commonStyles.row]}>
-          <View style={{ flexDirection: 'column', justifyContent: 'flex-end' }}>
-            {
-              showMembershipNumber &&
-              <View>
-                <Text
-                  style={[commonStyles.title, getFocusStyle(focus, 'memberNumber', '#828282')]}
-                >
-                  Membership number:
-                </Text>
-                <Text style={[getFocusStyle(focus, 'memberNumber', '#828282'), { fontSize: 18 }]}>
-                  {memberNumber}
-                </Text>
-              </View>
-            }
-          </View>
-        </View>
-      </View>
-      <View style={[commonStyles.back, bupaCardStyles.cardBack, { width, height, padding: 8 }]}>
-        <View style={[commonStyles.row]}>
-          {
-            showIssueNumber &&
-            <View>
-              <Text
-                style={[commonStyles.title, getFocusStyle(focus, 'issueNumber', '#828282'), {
-                  textAlign: 'right',
-                }]}
-              >
-                Card No: {issueNumber}
-              </Text>
-            </View>
-          }
-        </View>
-        {showSwipeBar && <View style={[commonStyles.bar, { backgroundColor: '#84cbff' }]} />}
-      </View>
-    </FlipCard>
-  );
+export const BupaCard = (props) => {
+  return GenericCard({ ...props, type: 'bupa', applyBackgroundImage: true });
+}
 
 BupaCard.propTypes = {
-  issueNumber: PropTypes.string,
+  title: PropTypes.string,
+  type: PropTypes.string,
+  logo: PropTypes.string,
+  logoStyle: PropTypes.object,
   cardHolderName: PropTypes.string,
   cardStyle: PropTypes.array,
   showSwipeBar: PropTypes.bool,
   width: PropTypes.number,
   height: PropTypes.number,
   focus: PropTypes.string,
+  issueDate: PropTypes.string,
+  issueNumberPosition: PropTypes.string,
+  issueDateText: PropTypes.string,
+  issueNumber: PropTypes.string,
+  issueNumberText: PropTypes.string,
   memberNumber: PropTypes.string,
+  memberNumberText: PropTypes.string,
   rank: PropTypes.string,
+  rankText: PropTypes.string,
   showCardHolderName: PropTypes.bool,
-  showIssueNumber: PropTypes.bool,
   showMembershipNumber: PropTypes.bool,
+  showIssueDate: PropTypes.bool,
   showRank: PropTypes.bool,
+  showIssueNumber: PropTypes.bool,
   showBack: PropTypes.bool,
+  applyBackgroundImage: PropTypes.bool,
+};
+
+BupaCard.defaultProps = {
+  title: 'HEALTH FUND',
+  logo: defaultLogo,
+  logoStyle: {},
 };
 
 export default BupaCard;
